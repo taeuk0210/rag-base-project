1ba10aec2a0444b677dd14a3ce797b1ca7ca8309
taeuk0210 <taeuk0210@naver.com>
2025-12-11 17:55:42 +0900

    chore: web initial commit

--- web/src/api/userApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const USER_URL = `http://localhost:7700/api/v1/users`;
-
-export const userApi = {
-  refresh: () =>
-    axios.post(`${USER_URL}/refresh`, {}, { withCredentials: true }),
-};
