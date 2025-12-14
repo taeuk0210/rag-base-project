139f7cff13866f8f46a448f4fe4192833c99cbc3
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2025-12-14 22:37:09 +0900

    feat: add cors setting

--- rag-api/app/api/v1/main.py
+++ rag-api/app/api/v1/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.endpoints import auth, document, chat
 
 app = FastAPI()
 
-app.include_router(document.router, prefix="/api/v1/docs", tags=["docs"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(document.router, prefix="/api/v1/doc", tags=["doc"])
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
-app.include_router(chat.router, prefix="/api/v1/chats", tags=["chats"])
+app.include_router(chat.router, prefix="/api/v1/chat", tags=["chat"])
